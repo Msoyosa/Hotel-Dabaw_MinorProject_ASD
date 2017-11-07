b6f5bcf2a429d2834917440f5c0e373348251928
karlasison1995 <karla.sison1995@gmail.com>
2017-11-07 12:06:58 +0800

    privacy_policy

---
+++ privacy_policy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Privacy Policy
+	</title>
+</head>
+<body>
+<div>
+	<?php 
+	echo "<h1>Hotel Dabaw's Privacy Policy</h1>";
+ 	?>	
+</div>
+
+
+</body>
+</html>
