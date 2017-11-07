fb821112698ee4dc1772a6a89a040f8cccedbbc2
karlasison1995 <karla.sison1995@gmail.com>
2017-11-07 12:08:22 +0800

    reservation_form.php

---
+++ reservation_form.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Reservation Form
+	</title>
+</head>
+<body>
+	<div>
+		<div>
+			<?php echo "Reservation Details (Required)" 
+			?>
+		</div>
+		<div>
+
+			<form method="post">
+
+			Check-in and Check-out: <input type="string" name="in_out" value=><br />
+			Number of Nights: <input type="integer" name="nights" value=""><br />	
+			Number of Rooms: <input type="integer" name="rooms" value=""><br />			     
+			Occupancy: <input type="text" name="nights" value="number of adult or underage occupants"><br />	
+			</form>
+
+			<form method="post">
+
+
+			</form>
+		</div>
+	</div>
+</body>
+</html>
