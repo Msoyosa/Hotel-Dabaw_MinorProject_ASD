b38530755192cc2ffab3a0b991bb69ed1fd4fbb9
roxetteortiz <roxette984@gmail.com>
2017-11-07 12:26:30 +0800

    contact.php

---
+++ contact.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Contact Hotel Dabaw
+	</title>
+</head>
+<body>
+<div>
+	<?php 
+	echo "<h1>How to contact Hotel Dabaw's management?</h1>";
+ 	?>	
+</div>
+</body>
+</html>
