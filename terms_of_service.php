92a279a0b6bdad4a44938b85cee0466ffc735bf1
Msoyosa <marjunesoyosa12@gmail.com>
2017-11-07 11:13:36 +0800

    terms_of_service.php

---
+++ terms_of_service.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Terms of Service
+	</title>
+</head>
+<body>
+	<div>
+	<?php 
+	echo "<h1>Hotel Dabaw's Terms of Service</h1>";
+ 	?>	
+</div>
+</body>
+</html>
