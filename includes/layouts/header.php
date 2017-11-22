f7238fd71660287c0b231072f615c1087f532701
Karla Sison <32417366+karlasison1995@users.noreply.github.com>
2017-11-21 17:18:46 -0800

    Add files via upload

--- includes/layouts/header.php
+++ includes/layouts/header.php
@@ -40,6 +40,9 @@
         	width: 500px;
         	margin-left: 400px;
         }
+        footer{
+            margin: 150px 0px 0px 0px;
+        }
 
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
